999af1a16bb1d3224c0fd17eaf6c562646b51101
ihijazi313 <ihijazi313@gmail.com>
2025-08-23 15:41:26 +0500

    feat(auth): integrate ConfigService for dynamic gRPC service URL
    - What: Updated the `main.ts` file to utilize `ConfigService` for retrieving the gRPC service URL dynamically. This change enhances the application's configuration management by allowing the service URL to be set via environment variables.
    - Why: To improve flexibility and maintainability by decoupling the service URL from the codebase, enabling easier updates and environment-specific configurations.
    - How: Modified the gRPC microservice connection options to fetch the `AUTH_GRPC_SERVICE_URL` from the `ConfigService`.
    - Notes: Ensure that the environment variable is correctly set in all deployment environments and verify that the application connects to the gRPC service as expected.

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app/app.module';
 import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { join } from 'node:path';
 import { init } from '@jobber-microservice/nestjs';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
@@ -15,6 +16,7 @@ async function bootstrap() {
   app.connectMicroservice<GrpcOptions>({
     transport: Transport.GRPC,
     options: {
+      url: app.get(ConfigService).getOrThrow('AUTH_GRPC_SERVICE_URL'),
       package: AUTH_PACKAGE_NAME,
       // protoPath: join(__dirname, 'proto/auth.proto'),
       // protoPath: join(__dirname, '../../libs/grpc/proto/auth.proto'),
